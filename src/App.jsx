a00256e17caff4623bc7245b4037a5baed25539c
akibrahman <akibrahman5200@gmail.com>
2023-09-24 21:08:20 +0600

    Project Created and tailwind, daisyUI installed and basic recources added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <p className="">Akib</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/DonationDetails/DonationDetails.jsx
@@ -0,0 +1,9 @@
+const DonationDetails = () => {
+  return (
+    <div>
+      <p>Donation Details</p>
+    </div>
+  );
+};
+
+export default DonationDetails;
